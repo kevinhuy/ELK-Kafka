d18da7eac54742a03eb57eade8cb234578249236
Calvin Remsburg <packetferret@gmail.com>
2020-06-28 17:44:36 -0500

    asdf

--- files/docker/python_consumers/python/consumer_ansible_callback.py
+++ files/docker/python_consumers/python/consumer_ansible_callback.py
@@ -3,77 +3,6 @@ from json import loads, dumps
 import requests
 import re
 
-def kafka_cleanup(each_message):
-    # ##########################################################
-    # ### each message
-    # ########################################################## 
-    ansible_message = {}
-
-    # ansible_host
-    try:
-        ansible_message["ansible_host"] = each_message.value["ansible_host"]
-    except KeyError:
-        pass
-
-    # ansible_result
-    try:
-        ansible_message["ansible_result"] = each_message.value["ansible_result"]
-    except KeyError:
-        pass
-
-    # ansible_task
-    try:
-        ansible_message["ansible_task"] = each_message.value["ansible_task"]
-    except KeyError:
-        pass
-
-    # message
-    try:
-        ansible_message["message"] = each_message.value["message"]
-    except KeyError:
-        pass
-
-    # status
-    try:
-        ansible_message["status"] = each_message.value["status"]
-    except KeyError:
-        pass
-
-    return(ansible_message)
-
-
-def was_diff(ansible_result):
-    diff = False
-    match = re.match(r"^TASK.*PRINT.*$", ansible_result)
-    if match:
-        diff = True
-    return(diff)
-
-
-def send_request(host_name, neighbor, iface):
-    try:
-        response = requests.post(
-            url="http://10.255.127.47/api/v2/job_templates/12/launch/",
-            headers={
-                "Authorization": "Basic YXV0b21hdGlvbjpqdW5pcGVyMTIz",
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            data=dumps({
-                "extra_vars": {
-                    "iface": iface,
-                    "host_name": host_name,
-                    "neighbor": neighbor
-                }
-            })
-        )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
-
-
 consumer = KafkaConsumer(
     'ansible_callback',
     value_deserializer=lambda m: loads(m.decode('utf-8')),
@@ -82,6 +11,4 @@ consumer = KafkaConsumer(
 
 
 for each_message in consumer:
-    ansible_message = kafka_cleanup(each_message)
-    print(type(ansible_message))
-    # ansible_message = loads(ansible_message)
+    print("hello world")
