347af5a9ab1b850e270034c0552398322709d26b
Calvin Remsburg <packetferret@gmail.com>
2020-06-28 10:07:32 -0500

    parsing json

--- files/docker/python_consumers/python/consumer_ansible_callback.py
+++ files/docker/python_consumers/python/consumer_ansible_callback.py
@@ -4,25 +4,20 @@ import requests
 import re
 
 def kafka_cleanup(each_message):
-    host_ip = each_message.value['host']
-    host_name = each_message.value['hostname']
-    msg = each_message.value['router_message']
-    return(host_ip, host_name, msg)
+    ansible_host = each_message.value['ansible_host']
+    ansible_result = each_message.value['ansible_result']
+    ansible_task = each_message.value['ansible_task']
+    message = each_message.value['message']
+    status = each_message.value['status']
+    return(ansible_host, ansible_result, ansible_task, message, status)
 
 
-def syslog_cleanup(msg):
-    match = re.match(r"^\[(?P<junos_string>.*)]", msg)
+def was_diff(ansible_result):
+    diff = False
+    match = re.match(r"^TASK.*PRINT.*$", ansible_result)
     if match:
-        junos_string = match.groupdict(['junos_string'])
-        for k,v in junos_string.items():
-            msg = v.split(" ")
-            neighbor = msg[2]
-            neighbor = neighbor.split("=")
-            neighbor = neighbor[1].replace('"','')
-            iface = msg[3]
-            iface = iface.split("=")
-            iface = iface[1].replace('"','')
-        return(neighbor, iface)
+        diff = True
+    return(diff)
 
 
 def send_request(host_name, neighbor, iface):
@@ -57,7 +52,8 @@ consumer = KafkaConsumer(
 
 
 for each_message in consumer:
-    # host_ip, host_name, msg = kafka_cleanup(each_message)
-    # neighbor, iface = syslog_cleanup(msg)
-    # send_request(host_name, neighbor, iface)
+    ansible_host, ansible_result, ansible_task, message, status = kafka_cleanup(each_message)
+    diff = was_diff(ansible_result)
+    if diff == True:
+        print('ansible_host: ' + ansible_host + '\nansible_result: ' + ansible_result + '\nansible_task: ' + ansible_task + '\nmessage: ' + message + '\nstatus: ' + status)
     print(each_message)
